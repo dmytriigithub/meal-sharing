bc1827e1ec6d09af0223a56dd7cc6a4f3ddf5231
Dmytrii Datskevych <datskevychdv@gmail.com>
2023-03-23 01:21:23 +0100

    Added files meal-sharing NodejsWeek2

--- src/backend/app.js
+++ src/backend/app.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const path = require("path");
 
 const mealsRouter = require("./api/meals");
+const reservationsRouter = require("./api/reservations");
 const buildPath = path.join(__dirname, "../../dist");
 const port = process.env.PORT || 3000;
 const cors = require("cors");
+
 
 // For week4 no need to look into this!
 // Serve the built client html
@@ -20,6 +22,7 @@ app.use(express.json());
 app.use(cors());
 
 router.use("/meals", mealsRouter);
+router.use("/reservations", reservationsRouter);
 
 //-------------------------------------------------------------
 
