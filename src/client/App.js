1f9cd4c600ddaa6976fa19ec54e0ceb884167627
Dmytrii Datskevych <datskevychdv@gmail.com>
2023-05-12 13:41:12 +0200

    Added files for HW react-react1-week3

--- src/client/App.js
+++ src/client/App.js
@@ -1,16 +1,23 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import TestComponent from "./components/TestComponent/TestComponent";
+import MealsList from "./components/mealsList/MealsList";
 
 function App() {
+
   return (
+
     <Router>
+
       <Route exact path="/">
-        <p>test</p>
+        <h1>Meal sharing</h1>
+        <Link to="/meals"><button>Meals</button></Link>
       </Route>
-      <Route exact path="/lol">
-        <p>lol</p>
+
+      <Route exact path="/meals">
+        <MealsList/>
       </Route>
+      
       <Route exact path="/test-component">
         <TestComponent></TestComponent>
       </Route>
---
+++ src/client/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,10 @@
+import img from './error-icon.svg';
+
+const ErrorMessage = () => {
+    return (
+        <h1>ERROR</h1>
+        // <img style={{ display: 'block', width: "250px", height: "250px",objectFit: 'contain', margin: "0 auto"}}  src={img} alt="Error"/>
+    )
+}
+
+export default ErrorMessage;
