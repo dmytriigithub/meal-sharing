b7233316bed4f887daeb57920c1376e00be7e2ab
Dmytrii Datskevych <datskevychdv@gmail.com>
2023-05-21 13:39:44 +0200

    Added changes in ReviewForm component

--- src/client/components/reviewForm/ReviewForm.js
+++ src/client/components/reviewForm/ReviewForm.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState} from "react";
 
 import useMealSharingService from "../../services/MealSharingService";
 
@@ -16,12 +16,6 @@ const ReviewForm = () => {
 
     const {postReview} = useMealSharingService();
 
-    useEffect(() => {
-        console.log(new Date().toJSON().slice(0, 10))
-    }, [])
-
-    
-
     const {isReviewFormVisible, setReviewFormVisible, mealId} = useContext(SingleMealContext);
 
     const postReviewData = () => {
@@ -32,10 +26,8 @@ const ReviewForm = () => {
             "description": description,
             "name": name,
         };
-        
 
         postReview(data);
-        
     }
 
     return (
@@ -46,7 +38,18 @@ const ReviewForm = () => {
                 <form className="reviewForm">
                     <input required placeholder="Name" type="text" className="reviewName" value={name} onChange={(e) => {setName(e.target.value)}}/>
                     <textarea required placeholder="Description" cols={255} rows={10} className="resevationPhone" value={description} onChange={(e) => {setDescription(e.target.value)}}/>
-                    <input required placeholder="Stars" type="number" className="reviewStars" value={stars} max={5} min={0} onChange={(e) => {setStars(e.target.value)}}/>
+                    <div className="rating-area">
+                        <input type="radio" id="star-5" name="rating" value="5" onChange={(e) => {setStars(e.target.value)}}/>
+                        <label htmlFor="star-5" title="Rating «5»"></label>	
+                        <input type="radio" id="star-4" name="rating" value="4" onChange={(e) => {setStars(e.target.value)}}/>
+                        <label htmlFor="star-4" title="Rating «4»"></label>    
+                        <input type="radio" id="star-3" name="rating" value="3" onChange={(e) => {setStars(e.target.value)}}/>
+                        <label htmlFor="star-3" title="Rating «3»"></label>  
+                        <input type="radio" id="star-2" name="rating" value="2" onChange={(e) => {setStars(e.target.value)}}/>
+                        <label htmlFor="star-2" title="Rating «2»"></label>    
+                        <input type="radio" id="star-1" name="rating" value="1" onChange={(e) => {setStars(e.target.value)}}/>
+                        <label htmlFor="star-1" title="Rating «1»"></label>
+                    </div>
                     <button className="reviewFormBtn" onClick={postReviewData}>Submit</button>
                 </form>
             </div>
